1cfdbb9838c888de7829cd2dd8d33ea61a7825b2
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-02-05 15:26:14 +0100

    ex03 ongoing

---
+++ ex03/AMateria.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 15:22:14 by fmoulin           #+#    #+#             */
+/*   Updated: 2026/02/05 15:25:38 by fmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+# include <string>
+# include <cstdlib>
+# include <iostream>
+
+class AMateria
+{
+protected:
+	std::string	_type;
+public:
+	AMateria(std::string const & type);
+
+	std::string const & getType() const; //Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
+
+#endif
