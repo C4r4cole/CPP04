7b8fc4cfc7711a3ac8d87cda1740bee65776ad2e
C4r4cole <c4r4cole.chevalerie@gmail.com>
2026-02-05 14:14:07 +0100

    ex01 done ?

--- ex00/Dog.cpp
+++ ex00/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 15:14:56 by fmoulin           #+#    #+#             */
-/*   Updated: 2026/02/04 17:38:13 by fmoulin          ###   ########.fr       */
+/*   Updated: 2026/02/05 12:20:07 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Dog::Dog()
 {
 	_type = "Dog";
-	std::cout	<< "An dog is born"
+	std::cout	<< "A dog is born"
 				<< std::endl;
 }
 
